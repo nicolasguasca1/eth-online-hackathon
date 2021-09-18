52b5af2df1454f79ea2602d7f5f78d89a55c151b
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-09-18 16:57:32 +0200

    Providers added to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import React from "react";
+
+import { MoralisProvider } from "react-moralis";
+import { ContextProvider } from "../Context";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+  },
+});
+
+const serverUrl = "https://gpcsccfs4eyy.grandmoralis.com:2053/server";
+const appId = "nDoAAbLEDLmP9TArw7fXMikJnSTiB4XJlCDkfo4L";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <ChakraProvider theme={theme}>
+        <ContextProvider>
+          <Component {...pageProps} />
+        </ContextProvider>
+      </ChakraProvider>
+    </MoralisProvider>
+  );
 }
-export default MyApp
+export default MyApp;
