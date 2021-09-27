41b76e03cb4eca152ab8503777320236b43b44e7
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-09-27 21:25:57 +0200

    Two displays on CallPage

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -4,26 +4,28 @@ import { Grid, makeStyles, Paper, Typography } from "@material-ui/core";
 import { SocketContext } from "../Store";
 import { useMoralis } from "react-moralis";
 
-const useStyles = makeStyles((theme) => ({
-  video: {
-    width: "550px",
-    [theme.breakpoints.down("xs")]: {
-      width: "300px"
-    }
-  },
-  gridContainer: {
-    borderRadius: "15",
-    justifyContent: "center",
-    [theme.breakpoints.down("xs")]: {
-      flexDirection: "column"
-    }
-  },
-  paper: {
-    padding: "10px",
-    border: "2px solid gray",
-    margin: "10px"
-  }
-}));
+import styles from "../styles/CallPage.module.css";
+
+// const useStyles = makeStyles((theme) => ({
+//   video: {
+//     width: "550px",
+//     [theme.breakpoints.down("xs")]: {
+//       width: "300px"
+//     }
+//   },
+//   gridContainer: {
+//     borderRadius: "15",
+//     justifyContent: "center",
+//     [theme.breakpoints.down("xs")]: {
+//       flexDirection: "column"
+//     }
+//   },
+//   paper: {
+//     padding: "10px",
+//     border: "2px solid gray",
+//     margin: "10px"
+//   }
+// }));
 
 const VideoPlayer = (props: any) => {
   const {
@@ -37,38 +39,77 @@ const VideoPlayer = (props: any) => {
   }: any = useContext(SocketContext);
   const { isAuthenticated, logout, user } = useMoralis();
 
-  const classes = useStyles();
+  // const classes = useStyles();
+
+  //   return (
+  //     <Grid>
+  //       {stream && (
+  //         <Paper>
+  //           <Grid item xs={12} md={6}>
+  //             {/* <Typography
+  //             variant="h5"
+  //             gutterBottom>
+  //               {user?.attributes.username}`s Meeting Room
+  //             </Typography> */}
+  //             <video playsInline muted ref={myVideo} autoPlay />
+  //           </Grid>
+  //         </Paper>
+  //       )}
+  //       {callAccepted && !callEnded && (
+  //         <Paper className={classes.paper}>
+  //           <Grid item xs={12} md={6}>
+  //             <Typography variant="h5" gutterBottom>
+  //               {call.name || "Name"}
+  //             </Typography>
+  //             <video
+  //               playsInline
+  //               ref={userVideo}
+  //               autoPlay
+  //               className={classes.video}
+  //             />
+  //           </Grid>
+  //         </Paper>
+  //       )}
+  //     </Grid>
+  //   );
+  // };
 
   return (
-    <Grid>
+    <div className={styles.grid_container}>
       {stream && (
-        <Paper>
-          <Grid item xs={12} md={6}>
-            {/* <Typography 
-            variant="h5" 
-            gutterBottom>
-              {user?.attributes.username}`s Meeting Room
-            </Typography> */}
-            <video playsInline muted ref={myVideo} autoPlay />
-          </Grid>
-        </Paper>
+        // <Paper>
+        //   <Grid item xs={12} md={6}>
+        //     {/* <Typography
+        //     variant="h5"
+        //     gutterBottom>
+        //       {user?.attributes.username}`s Meeting Room
+        //     </Typography> */}
+        //     <video playsInline muted ref={myVideo} autoPlay />
+        //   </Grid>
+        // </Paper>
+        <div>
+          <video playsInline muted ref={myVideo} autoPlay />
+        </div>
       )}
       {callAccepted && !callEnded && (
-        <Paper className={classes.paper}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" gutterBottom>
-              {call.name || "Name"}
-            </Typography>
-            <video
-              playsInline
-              ref={userVideo}
-              autoPlay
-              className={classes.video}
-            />
-          </Grid>
-        </Paper>
+        // <Paper className={classes.paper}>
+        //   <Grid item xs={12} md={6}>
+        //     <Typography variant="h5" gutterBottom>
+        //       {call.name || "Name"}
+        //     </Typography>
+        //     <video
+        //       playsInline
+        //       ref={userVideo}
+        //       autoPlay
+        //       className={classes.video}
+        //     />
+        //   </Grid>
+        // </Paper>
+        <div>
+          <video playsInline ref={userVideo} autoPlay />
+        </div>
       )}
-    </Grid>
+    </div>
   );
 };
 
